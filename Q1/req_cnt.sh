d4bf0c855c49e79ca1ff209038f278e857990aea
kelvin.yue <kyue1005@gmail.com>
2021-04-29 22:52:56 +0800

    Update script for Q1

--- Q1/req_cnt.sh
+++ Q1/req_cnt.sh
@@ -1,1 +1,23 @@
 #! /bin/bash
+
+usage() {
+    echo "Usage:"
+    echo "top10_host.sh [-f FILE]"
+    echo "Description:"
+    echo "FILE,the path of gziped log file."
+    exit -1
+}
+
+while getopts hf: opt; do
+  case "${opt}" in
+    f) FILE="${OPTARG}";;
+    h) usage;;
+    ?) usage;;
+  esac
+done
+
+if [ -f "${FILE}" ]; then
+  gzip -dc "${FILE}" | sed -r '/^\s*$/d' | wc -l
+else
+  echo "${FILE} not exist"
+fi
--- Q1/top_country.sh
+++ Q1/top_country.sh
@@ -1,1 +1,24 @@
 #! /bin/bash
+
+usage() {
+    echo "Usage:"
+    echo "top10_host.sh [-f FILE]"
+    echo "Description:"
+    echo "FILE,the path of gziped log file."
+    exit -1
+}
+
+while getopts hf: opt; do
+  case "${opt}" in
+    f) FILE="${OPTARG}";;
+    h) usage;;
+    ?) usage;;
+  esac
+done
+
+if [ -f "${FILE}" ]; then
+  TOP_IP=$(gzip -dc "${FILE}" | cut -d' ' -f1 | sort | uniq -c | sort -r | head -n 1 | cut -d' ' -f2)
+  whois ${TOP_IP} | grep -iE ^country:
+else
+  echo "${FILE} not exist"
+fi
