f6c516b0ce41980d2b7b645565e20acbd3ff49c8
kelvin.yue <kyue1005@gmail.com>
2021-04-29 22:44:44 +0800

    Add error handling for awssh.sh

--- Q2/awssh.sh
+++ Q2/awssh.sh
@@ -1,9 +1,27 @@
 #! /bin/bash
 
-NAME="${1}"
+usage() {
+    echo "Usage:"
+    echo "awssh.sh INSTANCE_NAME_TAG"
+    echo "Description:"
+    echo "INSTANCE_NAME_TAG, name tag of the ec2 instance"
+    exit -1
+}
 
-INSTANCE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${NAME}")
+INSTANCE_NAME_TAG="${1}"
 
-IP=$(echo ${INSTANCE} | jq -r '.Reservations | .[] | .Instances | .[] | .PublicIpAddress' | head -n 1)
+if [ -z ${INSTANCE_NAME_TAG} ]; then
+  usage
+fi
+
+INSTANCE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME_TAG}")
+RESERVATIONS=$(echo "${INSTANCE}" | jq -r '.Reservations | .[]')
+
+if [ -z "${RESERVATIONS}" ]; then
+  echo "Host not found"
+  exit -1
+fi
+
+IP=$(echo "${RESERVATIONS}" | jq -r '.Instances | .[] | .PublicIpAddress' | head -n 1)
 
 exec ssh ec2-user@${IP}
