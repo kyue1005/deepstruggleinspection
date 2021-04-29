d976ba347d68ca2ad5ce2ac3a2ff2ab5f2ac3f3d
kelvin.yue <kyue1005@gmail.com>
2021-04-29 21:30:35 +0800

    Update Q1 script

--- Q1/top10_host.sh
+++ Q1/top10_host.sh
@@ -1,1 +1,27 @@
 #! /bin/bash
+
+usage() {
+    echo "Usage:"
+    echo "top10_host.sh [-f FILE]"
+    echo "Description:"
+    echo "FILE,the path of gziped log file."
+    exit -1
+}
+
+while getopt ":f" opt; do
+  case "${opt}" in
+    f) FILE="${OPTARG}";;
+    h) usage;;
+    ?) usage;;
+  esac
+done
+
+echo $opt
+
+# if [ -z "${f}" ]; then
+#     usage
+# fi
+
+# if [ -f "${FILE}" ]; then
+#   gzip -d "${FILE}" | cut -d' ' -f1 | sort | uniq -c | sort -r | head -n 10
+# fi
