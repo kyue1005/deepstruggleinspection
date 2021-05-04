9dd600dcc3a3e30fbb26364fce2bda8746db0d82
kelvin.yue <kyue1005@gmail.com>
2021-05-04 22:04:04 +0800

    Update deploy init script

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,6 @@ echo  "Deploy terraform stack"
 (cd tf && terraform init)
 (cd tf && terraform plan -out tfplan -var-file="terraform.tfvars")
 (cd tf && terraform apply "tfplan")
+
+echo "ALB dns name"
+(cd tf && terraform state show 'module.url_shortener_alb.aws_lb.this[0]' | grep dns_name)
--- init.sh
+++ init.sh
@@ -3,3 +3,11 @@
 echo "Build base image"
 (cd packer && packer init aws-ubuntu.base.pkr.hcl)
 (cd packer && packer build -var-file="variables.pkvars.hcl" aws-ubuntu.base.pkr.hcl)
+
+echo  "Deploy terraform stack"
+(cd tf && terraform init)
+(cd tf && terraform plan -out tfplan -var-file="terraform.tfvars")
+(cd tf && terraform apply "tfplan")
+
+echo "ALB dns name"
+(cd tf && terraform state show 'module.url_shortener_alb.aws_lb.this[0]' | grep dns_name)
