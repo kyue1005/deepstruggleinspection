2cdf92c2050d335e58d33860026a2aca92e40631
kelvin.yue <kyue1005@gmail.com>
2021-05-10 09:52:14 +0800

    Fix letterbytes

--- Q3/shortener/route.go
+++ Q3/shortener/route.go
@@ -147,7 +147,7 @@ func (s *Shortener) redirect(w http.ResponseWriter, r *http.Request, ps httprout
 	http.Redirect(w, r, u.SrcUrl, http.StatusNotModified)
 }
 
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 const (
 	letterIdxBits = 6                    // 6 bits to represent a letter index
 	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
